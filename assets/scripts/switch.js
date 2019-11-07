83d764c32c56b7e7cef5ed6f28243138ed125c68
jafarlow <jacobafarlow@gmail.com>
2019-11-07 10:07:44 -0500

    Begin work on dark mode switch
    index.html
    - added a switch div
    - linked to JS for switch
    
    switch.js
    - copied in JS code for a dark mode switch
    
    index.scss
    - copied in a CSS variable for dark mode, utilizing the `@supports`
    feature
    - began testing background color and widths to troubleshoot the switch
      - Since there's more work to be done, this code is commented out

---
+++ assets/scripts/switch.js
@@ -0,0 +1,4 @@
+// dark mode switch
+document.getElementById('themeSwitch').addEventListener('change', function(event){
+  (event.target.checked) ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+});
